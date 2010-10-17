951036c7ead6d18f5ea2c1d743d57e15985cf461
Steffen Mueller <smueller@cpan.org>
2010-10-17 17:11:18 +0200

    Show stars with unknown colonies in yellow

--- examples/map/make_map.pl
+++ examples/map/make_map.pl
@@ -23,30 +23,59 @@ my $max_y = LacunaMap::DB::Stars->max_y+10;
 
 my ($xsize, $ysize) = ($max_x - $min_x + 1, $max_y - $min_y + 1);
 my $img = Imager->new(xsize => $xsize, ysize => $ysize);
-my $black = Imager::Color->new(0, 0, 0);
-my $red   = Imager::Color->new(255, 0, 0);
-my $green = Imager::Color->new(0, 255, 0);
-my $blue  = Imager::Color->new(0, 0, 255);
-my $white = Imager::Color->new(255, 255, 255);
-my $grey  = Imager::Color->new(180, 180, 180);
+my $black  = Imager::Color->new(0, 0, 0);
+my $red    = Imager::Color->new(255, 0, 0);
+my $yellow = Imager::Color->new(255, 255, 0);
+my $green  = Imager::Color->new(0, 255, 0);
+my $blue   = Imager::Color->new(0, 0, 255);
+my $white  = Imager::Color->new(255, 255, 255);
+my $grey   = Imager::Color->new(180, 180, 180);
 $img->box(filled => 1, color => $black);
 
-LacunaMap::DB::Stars->iterate(sub {
-  $img->setpixel(x => $_->x - $min_x, y => $_->y - $min_y, color => $grey);
-});
-
-LacunaMap::DB::Bodies->iterate(sub {
-  my $body = $_;
-  if (not defined $body->x or not defined $body->y) {
-    return;
+# stars with known-position bodies or no bodies
+LacunaMap::DB->iterate(<<'QUERY',
+  select stars.x, stars.y
+    from stars
+    left outer join bodies on stars.id = bodies.star_id
+    where (bodies.x is not NULL)          -- known-position body
+          or
+          (bodies.sql_primary_id is NULL) -- no bodies
+QUERY
+  sub {
+    my ($x, $y) = @$_;
+    my $color = $grey;
+    $img->setpixel(x => $x - $min_x, y => $y - $min_y, color => $color);
   }
-  my $color;
-  if ($body->empire_id && $body->empire_id == $my_empire_id) { $color = $green; }
-  elsif ($body->empire_id) { $color = $red; }
-  elsif ($body->type =~ /habitable/i) { $color = $blue; }
-  else { $color = $white; }
-  $img->setpixel(x => $body->x - $min_x, y => $body->y - $min_y, color => $color);
-});
+);
+
+# stars with unknown-position bodies
+LacunaMap::DB->iterate(<<'QUERY',
+  select stars.x, stars.y
+    from stars inner join bodies on stars.id = bodies.star_id
+    where bodies.x is NULL
+QUERY
+  sub {
+    my ($x, $y) = @$_;
+    my $color = $yellow;
+    $img->setpixel(x => $x - $min_x, y => $y - $min_y, color => $color);
+  }
+);
+
+LacunaMap::DB::Bodies->iterate(
+  'where bodies.x is not NULL',
+  sub {
+    my $body = $_;
+    if (not defined $body->x or not defined $body->y) {
+      return;
+    }
+    my $color;
+    if ($body->empire_id && $body->empire_id == $my_empire_id) { $color = $green; }
+    elsif ($body->empire_id) { $color = $red; }
+    elsif ($body->type =~ /habitable/i) { $color = $blue; }
+    else { $color = $white; }
+    $img->setpixel(x => $body->x - $min_x, y => $body->y - $min_y, color => $color);
+  }
+);
 
 
 $img->write(file => "map.png");
