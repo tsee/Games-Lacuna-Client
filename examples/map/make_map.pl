1e015dfca0c9d85168d103d280d07542cf8f2773
Steffen Mueller <smueller@cpan.org>
2010-10-18 17:53:26 +0200

    add sill star highlight feature

--- examples/map/make_map.pl
+++ examples/map/make_map.pl
@@ -7,10 +7,27 @@ use lib 'lib';
 use lib 'examples/map/lib';
 use LacunaMap::DB;
 
-my $DbFile = 'map.sqlite';
+our $DbFile = 'map.sqlite';
+our @HighlightStars;
 GetOptions(
   'd|dbfile=s' => \$DbFile,
+  'hl|highlight-star=s@' => \@HighlightStars,
 );
+
+my %highlight_star_names;
+my %highlight_star_ids;
+my $get_extra_star_info = '';
+if (@HighlightStars) {
+  $get_extra_star_info = ', stars.id, stars.name '; # hack!
+}
+foreach my $hl (@HighlightStars) {
+  if ($hl =~ /^\d+$/) {
+    $highlight_star_ids{$hl} = 1;
+  }
+  else {
+    $highlight_star_names{$hl} = 1;
+  }
+}
 
 LacunaMap::DB->import($DbFile);
 
@@ -33,8 +50,8 @@ my $grey   = Imager::Color->new(80, 80, 80);
 $img->box(filled => 1, color => $black);
 
 # stars with known-position bodies or no bodies
-LacunaMap::DB->iterate(<<'QUERY',
-  select stars.x, stars.y
+LacunaMap::DB->iterate(<<"QUERY",
+  select stars.x, stars.y$get_extra_star_info
     from stars
     left outer join bodies on stars.id = bodies.star_id
     where (bodies.x is not NULL)          -- known-position body
@@ -42,21 +59,33 @@ LacunaMap::DB->iterate(<<'QUERY',
           (bodies.sql_primary_id is NULL) -- no bodies
 QUERY
   sub {
-    my ($x, $y) = @$_;
+    my ($x, $y, $id, $name) = @$_;
     my $color = $grey;
+    if (defined $name and
+        (exists $highlight_star_names{$name} or
+         exists $highlight_star_ids{$id}))
+    {
+      $color = $green;
+    }
     $img->setpixel(x => $x - $min_x, y => $y - $min_y, color => $color);
   }
 );
 
 # stars with unknown-position bodies
-LacunaMap::DB->iterate(<<'QUERY',
-  select stars.x, stars.y
+LacunaMap::DB->iterate(<<"QUERY",
+  select stars.x, stars.y$get_extra_star_info
     from stars inner join bodies on stars.id = bodies.star_id
     where bodies.x is NULL
 QUERY
   sub {
-    my ($x, $y) = @$_;
+    my ($x, $y, $id, $name) = @$_;
     my $color = $yellow;
+    if (defined $name and
+        (exists $highlight_star_names{$name} or
+         exists $highlight_star_ids{$id}))
+    {
+      $color = $green;
+    }
     $img->setpixel(x => $x - $min_x, y => $y - $min_y, color => $color);
   }
 );
