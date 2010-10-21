2bed26c27d1355eda45e1bbe8a2cb488eaeccc21
Adam Bellaire <bellaire@ufl.edu>
2010-10-21 11:07:33 -0400

    Adding colony_worlds.pl example script, which presents options for colonization.

---
+++ examples/colony_worlds.pl
@@ -0,0 +1,82 @@
+#!/usr/bin/perl 
+use strict;
+use warnings;
+use Games::Lacuna::Client;
+
+my $client = Games::Lacuna::Client->new(
+uri      => 'https://us1.lacunaexpanse.com', # Or something...
+api_key  => 'your-api-key',
+name     => 'your-empire-name',
+password => 'your-password',
+#debug    => 1,
+);
+
+use Data::Dumper;
+
+my $data = $client->empire->view_species_stats();
+my $sortby = $ARGV[0] || 'score';
+
+# Get orbits
+my $min_orbit = $data->{species}->{min_orbit};
+my $max_orbit = $data->{species}->{max_orbit};
+
+# Get planets
+my $planets        = $data->{status}->{empire}->{planets};
+my $home_planet_id = $data->{status}->{empire}->{home_planet_id}; 
+my ($hx,$hy)       = @{$client->body(id => $home_planet_id)->get_status()->{body}}{'x','y'};
+
+# Get obervatories;
+my @observatories;
+for my $pid (keys %$planets) {
+    my $buildings = $client->body(id => $pid)->get_buildings()->{buildings};
+    push @observatories, grep { $buildings->{$_}->{url} eq '/observatory' } keys %$buildings;
+}
+
+print "Orbits: $min_orbit through $max_orbit\n";
+print "Observatory IDs: ".join(q{, },@observatories)."\n";
+
+# Find stars
+my @stars;
+for my $obs_id (@observatories) {
+    push @stars, @{$client->building( id => $obs_id, type => 'Observatory' )->get_probed_stars()->{stars}};
+}
+
+# Gather planet data
+my @planets;
+for my $star (@stars) {
+    push @planets, grep { (not defined $_->{empire}) && $_->{orbit} >= $min_orbit && $_->{orbit} <= $max_orbit && $_->{type} eq 'habitable planet' } @{$star->{bodies}};
+}
+
+# Calculate some planet metadata
+for my $p (@planets) {
+    $p->{distance} = sqrt(($hx - $p->{x})**2 + ($hy - $p->{y})**2);
+    $p->{water_score} = ($p->{water} - 5000) / 50;
+    $p->{size_score}  = (($p->{size} > 50 ? 50 : $p->{size} ) - 30) * 5;
+    $p->{ore_score}   = (scalar grep { $p->{ore}->{$_} > 1 } keys %{$p->{ore}}) * 5;
+    $p->{score}       = ($p->{water_score}+$p->{size_score}+$p->{ore_score})/3;
+}
+
+# Sort and print results
+for my $p (sort { $b->{$sortby} <=> $a->{$sortby} } @planets) {
+    my $d = $p->{distance};
+    print_bar();
+    printf "%-20s [%4s,%4s] (%3s)\nSize: %2d                   Colony Ship Travel Time:  %3.1f hours\nWater: %4d    Short Range Colony Ship Travel Time: %3.1f hours\n",
+        $p->{name},$p->{x},$p->{y},int($d),$p->{size},($d/5.23),$p->{water},($d/.12);
+    print_bar();
+    for my $ore (sort keys %{$p->{ore}}) {
+        printf "  %8s %4d",substr($ore,0,8),$p->{ore}->{$ore};
+        if ($ore eq 'chromite' or $ore eq 'gypsum' or $ore eq 'monazite' or $ore eq 'zircon') {
+            print "\n";
+        }
+    }
+    print_bar();
+    printf "Score: %3d%% [Size: %3d%%, Water: %3d%%, Ore: %3d%%]\n",@{$p}{'score','size_score','water_score','ore_score'};
+    print_bar();
+    print "\n"
+}
+
+sub print_bar {
+    print "-" x 78;
+    print "\n";
+}
+
