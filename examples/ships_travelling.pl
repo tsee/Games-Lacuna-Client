28919cbb9b56002490950e7ef6a3c742aadac6ed
Alexandr Ciornii <alexchorny@gmail.com>
2010-11-01 16:03:54 +0200

    start writing ships_travelling.pl

---
+++ examples/ships_travelling.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+#
+use strict;
+use warnings;
+my $cfg_file = shift(@ARGV) || 'lacuna.yml';
+unless ( $cfg_file and -e $cfg_file ) {
+	die "Did not provide a config file";
+}
+
+my $client = Games::Lacuna::Client->new(
+	cfg_file => $cfg_file,
+	# debug    => 1,
+);
