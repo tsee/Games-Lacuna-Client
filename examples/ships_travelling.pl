9f564bef3e5a1505ef13d14c0e9d8e7eb79d3e29
Paul <logrado@gmail.com>
2010-11-03 16:33:27 -0500

    Sort by arrival time. Tweak output format.

--- examples/ships_travelling.pl
+++ examples/ships_travelling.pl
@@ -33,16 +33,30 @@ foreach my $planet (values %planets_by_name) {
   push @spaceports, map  { $client->building(type => 'SpacePort', id => $_) } @b;
 }
 
+my @ships;
 foreach my $sp (@spaceports) {
   my $ships=$sp->view_ships_travelling();
-  my @ships=@{$ships->{ships_travelling}};
-  foreach my $ship (@ships) {
-    my $from=$ship->{from};
-    my $to=$ship->{to};
+  foreach my $ship ( @{$ships->{ships_travelling}} ) {
     ( my $date_arrives = $ship->{date_arrives} ) =~ s{^(\d+)\s+(\d+)\s+}{$2/$1/};
-    my $arrives = time2str('%Y/%m/%d %H:%M', str2time($date_arrives));
-    die unless ref($from) eq 'HASH';
-    die unless ref($to) eq 'HASH';
-    print $ship->{type_human},' from ',$from->{name},' to ',$to->{name}," arrives $arrives\n";
+    $ship->{date_arrives} = str2time($date_arrives);
+    push @ships, $ship;
   }
+}
+
+my $by_arrival = sub { $a->{date_arrives} <=> $b->{date_arrives} };
+
+foreach my $ship (sort $by_arrival @ships) {
+  my $from=$ship->{from};
+  my $to=$ship->{to};
+  my $arrives = time2str('%Y/%m/%d %H:%M', $ship->{date_arrives});
+  #my $hours = int( ( $ship->{date_arrives} - time() ) / 3600 );
+  my $hours = ( $ship->{date_arrives} - time() ) / 3600;
+  if ($hours >= 2) {
+      $hours = int $hours if $hours >= 2;
+  } else {
+      $hours = sprintf "%.1f", $hours;
+  }
+  die unless ref($from) eq 'HASH';
+  die unless ref($to) eq 'HASH';
+  print $ship->{type_human},' from ',$from->{name},' to ',$to->{name}," arrives in $hours hours ($arrives)\n";
 }
