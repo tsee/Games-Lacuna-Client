4d2dc7deca88c993f5f0e6a99df3936f9443f692
fireartist <fireartist@gmail.com>
2010-11-18 21:24:38 +0000

    script to list all plans in all PCCs

---
+++ examples/plans_report.pl
@@ -0,0 +1,65 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+use List::Util            (qw(first));
+use Games::Lacuna::Client ();
+
+my $cfg_file = shift(@ARGV) || 'lacuna.yml';
+unless ( $cfg_file and -e $cfg_file ) {
+	die "Did not provide a config file";
+}
+
+my $client = Games::Lacuna::Client->new(
+	cfg_file => $cfg_file,
+	# debug    => 1,
+);
+
+# Load the planets
+my $empire  = $client->empire->get_status->{empire};
+
+# reverse hash, to key by name instead of id
+my %planets = map { $empire->{planets}{$_}, $_ } keys %{ $empire->{planets} };
+
+# Scan each planet
+foreach my $name ( sort keys %planets ) {
+
+    # Load planet data
+    my $planet    = $client->body( id => $planets{$name} );
+    my $result    = $planet->get_buildings;
+    my $body      = $result->{status}->{body};
+    
+    my $buildings = $result->{buildings};
+
+    # Find the PPC
+    my $ppc_id = first {
+            $buildings->{$_}->{name} eq 'Planetary Command Center'
+    } keys %$buildings;
+    
+    my $ppc   = $client->building( id => $ppc_id, type => 'PlanetaryCommand' );
+    my $plans = $ppc->view_plans->{plans};
+    
+    next if !@$plans;
+    
+    printf "%s\n", $name;
+    print "=" x length $name;
+    print "\n";
+    
+    my $max_length = max map { length $_->{name} } @$plans;
+    
+    for my $plan (@$plans) {
+        printf "%${max_length}s, level %d",
+            $plan->{name},
+            $plan->{level};
+        
+        if ( $plan->{extra_build_level} ) {
+            printf "extra build level %d", $plan->{extra_build_level};
+        }
+        
+        print "\n";
+    }
+    
+    print "\n";
+}
