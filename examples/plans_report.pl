56b3b633238994a6c078530cf2f89f5cc911da64
Gavin <halkeye@gmail.com>
2010-11-18 21:55:28 -0800

    Add max to import list (to fix script so it compiles)

--- examples/plans_report.pl
+++ examples/plans_report.pl
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use FindBin;
 use lib "$FindBin::Bin/../lib";
-use List::Util            (qw(first));
+use List::Util            (qw(first max));
 use Games::Lacuna::Client ();
 
 my $cfg_file = shift(@ARGV) || 'lacuna.yml';
