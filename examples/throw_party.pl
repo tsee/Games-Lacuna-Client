bb12b85dff75bdf24b2bb5d88761616626f7b244
fireartist <fireartist@gmail.com>
2010-11-28 21:41:28 +0000

    default to all planets if no args given. also throw party in all parks on each planet

--- examples/throw_party.pl
+++ examples/throw_party.pl
@@ -2,7 +2,6 @@
 
 use strict;
 use warnings;
-use List::Util            (qw(first));
 use Games::Lacuna::Client ();
 use Getopt::Long          (qw(GetOptions));
 my $cfg_file;
@@ -24,17 +23,20 @@ GetOptions(
     'planet=s@' => \@planets,
 );
 
-usage() if !@planets;
-
 my $client = Games::Lacuna::Client->new(
 	cfg_file => $cfg_file,
 	 #debug    => 1,
 );
 
-my $empire  = $client->empire->get_status->{empire};
+my $empire = $client->empire->get_status->{empire};
 
 # reverse hash, to key by name instead of id
 my %planets = map { $empire->{planets}{$_}, $_ } keys %{ $empire->{planets} };
+
+# if no --planet args provided, run on all planets
+if (!@planets ) {
+    @planets = keys %planets;
+}
 
 for my $name (@planets) {
     # Load planet data
@@ -42,25 +44,30 @@ for my $name (@planets) {
     my $result    = $body->get_buildings;
     my $buildings = $result->{buildings};
     
-    # Find the first Park
-    my $park_id = first {
+    my @park_id = grep {
             $buildings->{$_}->{name} eq 'Park'
     } keys %$buildings;
     
-    my $park = $client->building( id => $park_id, type => 'Park' );
-    
-    next unless $park->view->{party}{can_throw};
-    
-    $park->throw_a_party;
+    for my $park_id (@park_id) {
+        my $park = $client->building( id => $park_id, type => 'Park' );
+        
+        next unless $park->view->{party}{can_throw};
+        
+        $park->throw_a_party;
+    }
 }
 
 
 sub usage {
   die <<"END_USAGE";
 Usage: $0 throw_party.yml
-       --planet       NAME  (required)
+       --planet       NAME
 
 --planet can be passed multiple times.
+
+If planet is not provided, will attempt to throw a party on all planets.
+
+Throws a party in all parks on the planet(s).
 
 END_USAGE
 
