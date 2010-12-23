eabfb5450ccc8ddc6a8ad2bd223b404f6709a894
Brad Gilbert <b2gills@gmail.com>
2010-12-23 12:59:08 -0600

    Switched build_types.pl to use building data

--- data/build_types.pl
+++ data/build_types.pl
@@ -2,15 +2,19 @@
 use strict;
 use warnings;
 use YAML::Tiny;
-use FindBin;
 use File::Spec::Functions qw' abs2rel catfile ';
 use Template;
 
-my $input         = "${FindBin::Bin}/types.yml",
-my $output        = "${FindBin::Bin}/../lib/Games/Lacuna/Client/Types.pm",
-my $package       = 'Games::Lacuna::Client::Types',
-my $template_name = 'data/Types.tt2';
-my $generator     = "data/${FindBin::Script}";
+use FindBin;
+use lib "${FindBin::Bin}";
+require LoadBuilding;
+
+my $input          = "${FindBin::Bin}/types.yml";
+my $building_input = "${FindBin::Bin}/building.yml";
+my $output         = "${FindBin::Bin}/../lib/Games/Lacuna/Client/Types.pm";
+my $package        = 'Games::Lacuna::Client::Types';
+my $template_name  = 'data/Types.tt2';
+my $generator      = "data/${FindBin::Script}";
 
 my $template = abs2rel catfile $FindBin::Bin, 'Types.tt2';
 
@@ -22,11 +26,13 @@ unless( $yaml ){
 my $tt = Template->new({
 });
 
+my $types = LoadBuilding->Load($building_input)->types;
+
 my $vars = {
   generator     => $generator,
   package       => $package,
   resource      => $yaml->[0]{resource},
-  building_meta => $yaml->[0]{building_meta},
+  building_meta => $types,
   template_name => $template_name,
 };
 
