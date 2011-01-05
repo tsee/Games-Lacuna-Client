184c33538578412daedcdeb782df384be08de480
Brad Gilbert <b2gills@gmail.com>
2011-01-04 18:28:37 -0600

    Incorrect sprintf arg in examples/glyph_prices.pl

--- examples/glyph_prices.pl
+++ examples/glyph_prices.pl
@@ -148,7 +148,7 @@ sub stringify{
   if( $n == int $n ){
     return sprintf '%2i   ', $n;
   }else{
-    my $str = sprintf '%2.2f', $n;
+    my $str = sprintf '%5.2f', $n;
     if( $str =~ /\.00/ ){
       return sprintf '%2i   ', $n;
     }
